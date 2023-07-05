8d80fd417be8b52439a1892634d7c18b70f6835a
izbr <ibasquerice@gmail.com>
2023-07-05 17:12:39 +0100

    re-added .vim

--- vim/.vim/templates/skeleton.py
+++ vim/.vim/templates/skeleton.py
@@ -7,3 +7,11 @@ Auth: Isaac Basque-Rice
 Date: 
 """
 
+def main():
+    """ Main entry point of the app """
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
